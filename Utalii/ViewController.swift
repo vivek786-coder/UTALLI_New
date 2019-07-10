8e9d10a9a123c26959893babd26fc60909c4d53c
Mohammad Yunus <mohammad@virtualemployee.com>
2019-07-10 11:29:58 +0530

    First commit

---
+++ Utalii/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Utalii
+//
+//  Created by Vivek kumar on 7/9/19.
+//  Copyright © 2019 Vivek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
